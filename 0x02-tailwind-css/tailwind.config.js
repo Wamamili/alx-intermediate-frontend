6f1e101bf9b49f68599cc40b1c4f2dd3b0ab11ba
wamamili <wamamiliclean@gmail.com>
2025-09-10 19:30:50 +0300

    Responsive CSS Grid Layout

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./*.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
